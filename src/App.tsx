b4ce0477c8dde6a6f329ed9d7d1effba71a16734
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-09-17 12:00:06 -0300

    feat: add side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { Outlet } from 'react-router-dom'
-import Titlebar from './components/Titlebar.tsx'
+import { Outlet } from "react-router-dom";
+import Titlebar from "./components/Titlebar.tsx";
+import { useState } from "react";
+import Menu from "./components/Menu.tsx";
 
 function App() {
-  return <div className="flex flex-col min-h-screen">
-    <Titlebar />
-    <Outlet />  
-  </div>
+  const [isFileTreeOpen, setIsFileTreeOpen] = useState(true);
 
+  return (
+    <div className="flex flex-col h-screen">
+      <Titlebar />
+      <div className="flex h-full">
+        <Menu
+          isFileTreeOpen={isFileTreeOpen}
+          openCloseFileTree={() => setIsFileTreeOpen(!isFileTreeOpen)}
+        />
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,75 @@
+import { FC } from "react";
+import { LuFolderTree } from "react-icons/lu";
+import {
+  PiNotePencil,
+  PiClockCounterClockwiseLight,
+  PiQuestionLight,
+} from "react-icons/pi";
+import { HiOutlineCog } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+
+interface MenuProps {
+  isFileTreeOpen: boolean;
+  openCloseFileTree: () => void;
+}
+
+const Menu: FC<MenuProps> = ({ isFileTreeOpen, openCloseFileTree }) => {
+  const location = useLocation();
+
+  const routes = [
+    {
+      href: "/",
+      icon: <PiNotePencil className="h-5" />,
+      tooltip: "Anotações",
+      isActive: location.pathname === "/",
+    },
+    {
+      href: "/repeticao",
+      icon: <PiClockCounterClockwiseLight className="h-5" />,
+      tooltip: "Repetição Espaçada",
+      isActive: location.pathname === "/repeticao",
+    },
+    {
+      href: "/ajuda",
+      icon: <PiQuestionLight className="h-5" />,
+      tooltip: "Ajuda",
+      isActive: location.pathname === "/ajuda",
+    },
+    {
+      href: "/configuracoes",
+      icon: <HiOutlineCog className="h-5" />,
+      tooltip: "Configurações",
+      isActive: location.pathname === "/configuracoes",
+    },
+  ];
+
+  return (
+    <aside className="h-full w-9 bg-white/90 flex flex-col justify-between border-r border-r-zinc-300">
+      <div>
+        <button
+          className={`w-full p-2 flex justify-center hover:bg-zinc-300/75 group ${
+            isFileTreeOpen ? "bg-zinc-300/75" : ""
+          }`}
+          onClick={openCloseFileTree}
+        >
+          <LuFolderTree className="text-zinc-800" />
+        </button>
+      </div>
+
+      <nav className="flex flex-col text-zinc-800 w-full">
+        {routes.map((route) => (
+          <Link
+            to={route.href}
+            className={`flex justify-center hover:bg-zinc-300/75 p-2 ${
+              route.isActive ? "bg-zinc-300/75" : ""
+            }`}
+          >
+            {route.icon}
+          </Link>
+        ))}
+      </nav>
+    </aside>
+  );
+};
+
+export default Menu;
