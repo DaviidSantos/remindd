e6cef174f4ffa6b053d97ff3568682f78532faa9
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-09-17 10:30:00 -0300

    feat: add titlebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import Titlebar from './components/Titlebar.tsx'
 
 function App() {
-  return <Outlet />
+  return <div className="flex flex-col min-h-screen">
+    <Titlebar />
+    <Outlet />  
+  </div>
 
 }
 
