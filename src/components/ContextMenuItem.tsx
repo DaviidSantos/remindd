a46ab116dfc87f72378ba5111ec0e1d5a695bf9d
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-09-23 13:44:43 -0300

    feat: add close note on delete

--- src/components/ContextMenuItem.tsx
+++ src/components/ContextMenuItem.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { IconType } from "react-icons";
 import { useFileTreeContext } from "../context/FileTreeContext";
+import { useNotesContext } from "../context/NotesContext";
 
 interface ContextMenuItemProps {
   icon: IconType;
@@ -16,9 +17,15 @@ const ContextMenuItem: FC<ContextMenuItemProps> = ({
   action,
 }) => {
   const { readFileTree } = useFileTreeContext();
+  const { notes, setNotes, activeNote, setActiveNote } = useNotesContext();
 
   const itemClick = async () => {
     await action(path);
+    const updatedNotes = notes.filter((note) => note.path !== path);
+    setNotes(updatedNotes);
+    if (activeNote !== 0) {
+      setActiveNote(activeNote - 1);
+    }
     readFileTree();
   };
 
